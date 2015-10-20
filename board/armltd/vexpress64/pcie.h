ab772e90ae970bb650c31363478f8dde4dc578ea
Linus Walleij <linus.walleij@linaro.org>
2015-10-20 08:05:40 +0200

    vexpress64: compile Juno PCIe conditionally
    Only compile in PCIe support if the board really uses it. Provide
    a stub for the init function if e.g. FVP is being built.
    
    Cc: Liviu Dudau <Liviu.Dudau@foss.arm.com>
    Cc: Ryan Harkin <ryan.harkin@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- board/armltd/vexpress64/pcie.h
+++ board/armltd/vexpress64/pcie.h
@@ -1,6 +1,10 @@
 #ifndef __VEXPRESS64_PCIE_H__
 #define __VEXPRESS64_PCIE_H__
 
+#ifdef CONFIG_TARGET_VEXPRESS64_JUNO
 void vexpress64_pcie_init(void);
+#else
+static inline void vexpress64_pcie_init(void) {}
+#endif
 
 #endif /* __VEXPRESS64_PCIE_H__ */
