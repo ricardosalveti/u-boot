08dfd86a8fc7dacf0aa2824995a09b90d33b05e1
Ryan Harkin <ryan.harkin@linaro.org>
2017-03-02 18:15:43 +0000

    vexpress64: semi: move initrd_addr to 0x84000000
    To match the address used the the DRAM config.
    
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- include/configs/vexpress_aemv8a.h
+++ include/configs/vexpress_aemv8a.h
@@ -174,7 +174,7 @@
 				"kernel_name=Image\0"		\
 				"kernel_addr=0x80080000\0"	\
 				"initrd_name=ramdisk.img\0"	\
-				"initrd_addr=0x88000000\0"	\
+				"initrd_addr=0x84000000\0"	\
 				"fdtfile=devtree.dtb\0"		\
 				"fdt_addr=0x83000000\0"		\
 				"fdt_high=0xffffffffffffffff\0"	\
