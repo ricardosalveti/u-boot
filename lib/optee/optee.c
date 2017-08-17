d19645817a26209f9ff16c2f70ad28e2cf09dd86
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2017-08-17 22:23:05 +0100

    optee: Add lib entries for sharing OPTEE code across ports
    This patch adds code to lib to enable sharing of useful OPTEE code between
    board-ports and architectures. The code on lib/optee/optee.c comes from the
    TI omap2 port. Eventually the OMAP2 code will be patched to include the
    shared code. The intention here is to add more useful OPTEE specific code
    as more functionality get added.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

---
+++ lib/optee/optee.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2017 Linaro
+ * Bryan O'Donoghue <bryan.odonoghue@linaro.org>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <tee/optee.h>
+
+int optee_verify_image(struct optee_header *hdr, unsigned long tzdram_start,
+		       unsigned long tzdram_len, unsigned long image_len)
+{
+	unsigned long tzdram_end = tzdram_start + tzdram_len;
+	uint32_t tee_file_size;
+
+	tee_file_size = hdr->init_size + hdr->paged_size +
+			sizeof(struct optee_header);
+
+	if ((hdr->magic != OPTEE_MAGIC) ||
+	    (hdr->version != OPTEE_VERSION) ||
+	    (hdr->init_load_addr_hi > tzdram_end) ||
+	    (hdr->init_load_addr_lo < tzdram_start) ||
+	    (tee_file_size > tzdram_len) ||
+	    (tee_file_size != image_len) ||
+	    ((hdr->init_load_addr_lo + tee_file_size) > tzdram_end)) {
+		printf("OPTEE verification error tzdram 0x%08lx-0x%08lx "
+		       "header lo=0x%08x hi=0x%08x size=0x%08x\n",
+		       tzdram_start, tzdram_end, hdr->init_load_addr_lo,
+		       hdr->init_load_addr_hi, tee_file_size);
+		return -EINVAL;
+	}
+
+	return 0;
+}
