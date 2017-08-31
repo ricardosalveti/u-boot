ab276c1ad5818057643c84192372bd31f7108d90
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2017-08-31 10:59:38 +0100

    warp7: optee: Define an optee_file
    Adds a variable to the environment which allows us to change the name of
    the OPTEE file to be loaded.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- include/configs/warp7.h
+++ include/configs/warp7.h
@@ -37,6 +37,7 @@
 	"initrd_high=0xffffffff\0" \
 	"fdt_file=imx7s-warp.dtb\0" \
 	"fdt_addr=0x83000000\0" \
+	"optee_file=uTee.optee\0" \
 	"optee_addr=0x84000000\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
