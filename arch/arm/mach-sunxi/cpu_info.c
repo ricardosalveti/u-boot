2ec7b22ffb592b70c3fb351113da8cccf89b87c2
Jun Nie <jun.nie@linaro.org>
2018-01-19 21:36:46 +0800

    sunxi: support fuse cmd to read/write fuse
    Support fuse cmd to read/write fuse. Power supply for fuse
    should be ready, name is VDD_EFUSE in some schematic.
    
    Signed-off-by: Jun Nie <jun.nie@linaro.org>

--- arch/arm/mach-sunxi/cpu_info.c
+++ arch/arm/mach-sunxi/cpu_info.c
@@ -105,7 +105,7 @@ int print_cpuinfo(void)
 }
 #endif
 
-#ifdef CONFIG_MACH_SUN8I_H3
+#if defined(MACH_SUNXI_H3_H5) || defined(CONFIG_MACH_SUN50I)
 
 #define SIDC_PRCTL 0x40
 #define SIDC_RDKEY 0x60
@@ -133,13 +133,37 @@ uint32_t sun8i_efuse_read(uint32_t offset)
 	reg_val = readl(SUNXI_SIDC_BASE + SIDC_RDKEY);
 	return reg_val;
 }
+
+uint32_t sun8i_efuse_write(u32 offset, u32 val)
+{
+	u32 reg_val;
+
+	writel(val, SUNXI_SIDC_BASE + SIDC_RDKEY);
+
+	reg_val = readl(SUNXI_SIDC_BASE + SIDC_PRCTL);
+	reg_val &= ~(((0x1ff) << 16) | 0x3);
+	reg_val |= (offset << 16);
+	writel(reg_val, SUNXI_SIDC_BASE + SIDC_PRCTL);
+
+	reg_val &= ~(((0xff) << 8) | 0x3);
+	reg_val |= (SIDC_OP_LOCK << 8) | 0x1;
+	writel(reg_val, SUNXI_SIDC_BASE + SIDC_PRCTL);
+
+	while (readl(SUNXI_SIDC_BASE + SIDC_PRCTL) & 0x1)
+		;
+
+	reg_val &= ~(((0x1ff) << 16) | ((0xff) << 8) | 0x3);
+	writel(reg_val, SUNXI_SIDC_BASE + SIDC_PRCTL);
+
+	return 0;
+}
 #endif
 
 int sunxi_get_sid(unsigned int *sid)
 {
 #ifdef CONFIG_AXP221_POWER
 	return axp_get_sid(sid);
-#elif defined CONFIG_MACH_SUN8I_H3
+#elif defined(MACH_SUNXI_H3_H5) || defined(CONFIG_MACH_SUN50I)
 	/*
 	 * H3 SID controller has a bug, which makes the initial value of
 	 * SUNXI_SID_BASE at boot wrong.
@@ -163,4 +187,40 @@ int sunxi_get_sid(unsigned int *sid)
 #else
 	return -ENODEV;
 #endif
+}
+
+int fuse_read(u32 bank, u32 word, u32 *sid)
+{
+#if defined(MACH_SUNXI_H3_H5) || defined(CONFIG_MACH_SUN50I)
+	*sid = sun8i_efuse_read(word);
+#elif defined SUNXI_SID_BASE
+	*sid = readl((ulong)SUNXI_SID_BASE + word);
+#else
+	return -ENODEV;
+#endif
+	return 0;
+}
+
+int fuse_prog(u32 bank, u32 word, u32 val)
+{
+#if defined(MACH_SUNXI_H3_H5) || defined(CONFIG_MACH_SUN50I)
+	return sun8i_efuse_write(word, val);
+#elif defined SUNXI_SID_BASE
+	writel(val, (ulong)SUNXI_SID_BASE + word);
+#else
+	return -ENODEV;
+#endif
+	return 0;
+}
+
+int fuse_sense(u32 bank, u32 word, u32 *val)
+{
+	/* We do not support sensing :-( */
+	return -EINVAL;
+}
+
+int fuse_override(u32 bank, u32 word, u32 val)
+{
+	/* We do not support overriding :-( */
+	return -EINVAL;
 }
