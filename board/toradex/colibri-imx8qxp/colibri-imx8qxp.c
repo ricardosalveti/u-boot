32f479cd9a823056fa2689a6e92759893b3b62d4
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2019-03-20 09:47:46 +0100

    colibri-imx8qxp: adjust copyright/licensing headers
    Adjust copyright/licensing headers e.g. universally using SPDX license
    identifier.
    
    Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>

--- board/toradex/colibri-imx8qxp/colibri-imx8qxp.c
+++ board/toradex/colibri-imx8qxp/colibri-imx8qxp.c
@@ -1,7 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright (C), 2018 Toradex AG
- *
- * SPDX-License-Identifier:	GPL-2.0+
+ * Copyright 2018-2019 Toradex
  */
 #include <common.h>
 #include <malloc.h>
