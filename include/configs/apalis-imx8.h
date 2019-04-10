365994c527e861ef6ca833197802b3464a89d805
Max Krummenacher <max.krummenacher@toradex.com>
2019-04-10 16:12:35 +0200

    apalis-imx8: adjust copyright/licensing headers
    Adjust copyright/licensing headers e.g. universally using SPDX license
    identifier.
    
    Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

--- include/configs/apalis-imx8.h
+++ include/configs/apalis-imx8.h
@@ -1,8 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * Copyright 2017 NXP
- * Copyright 2017 Toradex
- *
- * SPDX-License-Identifier:	GPL-2.0+
+ * Copyright 2017-2019 Toradex
  */
 
 #ifndef __APALIS_IMX8QM_H
