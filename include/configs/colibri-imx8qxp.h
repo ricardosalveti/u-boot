c06c3466cf1761617cd262e6dbae3f5d79c2e6a9
Stefan Agner <stefan.agner@toradex.com>
2019-05-09 17:30:36 +0200

    colibri-imx8x: enable FDT relocation
    FDT relocation disabled seems to cause issues when using TorizonCore,
    probably related to ramdisk.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- include/configs/colibri-imx8qxp.h
+++ include/configs/colibri-imx8qxp.h
@@ -91,7 +91,6 @@
 	"panel=NULL\0" \
 	"console=ttyLP3,115200 earlycon=lpuart32,0x5a090000,115200\0" \
 	"fdt_addr=0x83000000\0"			\
-	"fdt_high=0xffffffffffffffff\0"		\
 	"boot_fdt=try\0" \
 	"fdt_file=fsl-imx8qxp-colibri-eval-v3.dtb\0" \
 	"initrd_addr=0x83800000\0"		\
