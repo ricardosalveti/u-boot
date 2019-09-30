bb64b46fb79073b4368b0cdf0fe9d6e77ddee155
Philippe Schenker <philippe.schenker@toradex.com>
2019-09-30 16:20:47 +0200

    include: apalis-imx8.h: change tdxargs to defargs for pci_nomsi
    tdxargs are meant for temporary adjusting U-Boot. Use defargs
    that suits this purpose better.
    
    Signed-off-by: Philippe Schenker <philippe.schenker@toradex.com>

--- include/configs/apalis-imx8.h
+++ include/configs/apalis-imx8.h
@@ -127,7 +127,7 @@
 	"mmcargs=setenv bootargs console=${console},${baudrate} root=${mmcroot} " \
 	"video=imxdpufb5:off video=imxdpufb6:off video=imxdpufb7:off\0" \
 	"setup=run loadhdp; hdp load ${hdp_addr}; run mmcargs\0" \
-	"tdxargs=pci=nomsi"
+	"defargs=pci=nomsi"
 
 /* Link Definitions */
 #define CONFIG_LOADADDR			0x80280000
