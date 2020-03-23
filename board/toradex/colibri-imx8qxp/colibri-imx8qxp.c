890d03ae33a2fa62a1bd577ba54cbf3c945cccef
Igor Opaniuk <igor.opaniuk@toradex.com>
2020-03-23 19:23:47 +0200

    colibri-imx8qxp: sync with MEK platform
    1. Enable CONFIG_SNVS_SEC_SC_AUTO by default
    2. Move BOOTAUX_* defines to defconfig
    
    Related-to: ELB-2605
    Signed-off-by: Igor Opaniuk <igor.opaniuk@toradex.com>

--- board/toradex/colibri-imx8qxp/colibri-imx8qxp.c
+++ board/toradex/colibri-imx8qxp/colibri-imx8qxp.c
@@ -18,6 +18,7 @@
 #include <asm/arch/clock.h>
 #include <asm/mach-imx/sci/sci.h>
 #include <asm/arch/imx8-pins.h>
+#include <asm/arch/snvs_security_sc.h>
 #include <dm.h>
 #include <imx8_hsio.h>
 #include <usb.h>
@@ -335,6 +336,15 @@ int board_init(void)
 
 #ifdef CONFIG_TDX_CMD_IMX_MFGR
 	(void) pmic_init();
+#endif
+
+#ifdef CONFIG_SNVS_SEC_SC_AUTO
+	{
+		int ret = snvs_security_sc_init();
+
+		if (ret)
+			return ret;
+	}
 #endif
 
 	return 0;
