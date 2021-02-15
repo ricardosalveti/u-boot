9ed1448d5f16cabf3d554f0ba3226728a086922b
Igor Opaniuk <igor.opaniuk@foundries.io>
2021-02-15 19:46:02 +0200

    [FIO internal] ARM: imx8m: get/set the primary/secondary bmode
    Add new getprisec() function, which returns 0 if the system booted
    from primary copy or to 1 if the system booted from secondary copy,
    by reading PERSIST_SECONDARY_BOOT bit from GPR10 register.
    
    Add new boot_mode_enable_secondary() function, which adjusts
    PERSIST_SECONDARY_BOOT bit from GPR10 register so BootROM can boot
    into the right boot path: primary or secondary.
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

--- arch/arm/mach-imx/imx8m/soc.c
+++ arch/arm/mach-imx/imx8m/soc.c
@@ -774,6 +774,24 @@ int spl_mmc_emmc_boot_partition(struct mmc *mmc)
 }
 #endif
 
+int boot_mode_getprisec(void)
+{
+	struct src *psrc = (struct src *)SRC_BASE_ADDR;
+
+	return !!(readl(&psrc->gpr10) & SRC_GPR10_PERSIST_SECONDARY_BOOT);
+}
+
+void boot_mode_enable_secondary(bool enable)
+{
+	u32 persist_sec = SRC_GPR10_PERSIST_SECONDARY_BOOT;
+	struct src *psrc = (struct src *)SRC_BASE_ADDR;
+
+	if (enable)
+		setbits_le32(&psrc->gpr10, persist_sec);
+	else
+		clrbits_le32(&psrc->gpr10, persist_sec);
+}
+
 bool is_usb_boot(void)
 {
 	return get_boot_device() == USB_BOOT;
