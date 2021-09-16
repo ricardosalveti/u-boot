8ad8053e4be79a7de310f17245714c87bdc46b6a
Henry Beberman <hebeberm@microsoft.com>
2021-09-16 16:09:57 +0300

    [FIO fromlist] spl: Add CONFIG_SPL_FIT_SIGNATURE_STRICT
    SPL FIT load checks the signature on loadable images but just continues
    in the case of a failure. This is undesirable behavior because the boot
    process depends on the authenticity of each loadable part.
    
    Adding CONFIG_SPL_FIT_SIGNATURE_STRICT to halt the platform when any
    image fails its signature check, including loadable parts.
    
    SPL already supports image signature verification but had no mechanism
    to check that the FIT's configuration block was signed correctly.
    
    Add a check near the start of spl_load_simple_fit that verifies the
    FIT's configuration block, and fails if it's not present or the
    signature doesn't match what's stored in the SPL DTB.
    
    Signed-off-by: Henry Beberman <hebeberm@microsoft.com>
    Co-developed-by: Ricardo Salveti <ricardo@foundries.io>
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>
    Co-developed-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

--- common/spl/spl_fit.c
+++ common/spl/spl_fit.c
@@ -8,6 +8,7 @@
 #include <errno.h>
 #include <fpga.h>
 #include <gzip.h>
+#include <hang.h>
 #include <image.h>
 #include <log.h>
 #include <memalign.h>
@@ -322,8 +323,14 @@ static int spl_load_fit_image(struct spl_load_info *info, ulong sector,
 		printf("## Checking hash(es) for Image %s ... ",
 		       fit_get_name(fit, node, NULL));
 		if (!fit_image_verify_with_data(fit, node, gd_fdt_blob(), src,
-						length))
-			return -EPERM;
+						length)) {
+			if (CONFIG_IS_ENABLED(FIT_SIGNATURE_STRICT)) {
+				puts("Invalid FIT signature found in a required image.\n");
+				hang();
+			} else {
+				return -EPERM;
+			}
+		}
 		puts("OK\n");
 	}
 
@@ -719,8 +726,14 @@ int spl_load_simple_fit(struct spl_image_info *spl_image,
 	ctx.fit = spl_load_simple_fit_fix_load(ctx.fit);
 
 	ret = spl_simple_fit_parse(&ctx);
-	if (ret < 0)
-		return ret;
+	if (ret < 0) {
+		if (CONFIG_IS_ENABLED(FIT_SIGNATURE_STRICT)) {
+			puts("SPL_FIT_SIGNATURE_STRICT needs a valid config node in FIT\n");
+			hang();
+		} else {
+			return ret;
+		}
+	}
 
 	if (IS_ENABLED(CONFIG_SPL_FPGA))
 		spl_fit_load_fpga(&ctx, info, sector);
