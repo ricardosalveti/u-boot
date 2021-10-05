arduino/u-boot-imx

commit 545d964536327abdd3c0b9e6272d80b49b0737e2

[FIO extras] fiovb: return error on init if board not closed
Signed-off-by: Tim Anderson <tim.anderson@foundries.io>

cmd/fiovb.c
@@ -20,6 +20,9 @@ int do_fiovb_init(struct cmd_tbl *cmdtp, int flag, int argc, char * const argv[]
 	if (argc != 2)
 		return CMD_RET_USAGE;
 
+	if (!boot_mode_is_closed())
+		return CMD_RET_FAILURE;
+
 	mmc_dev = simple_strtoul(argv[1], NULL, 16);
 
 	if (fiovb_ops)

arch/arm/mach-imx/misc.c
@@ -213,3 +213,8 @@ int add_res_mem_dt_node(void *fdt, const char *name, phys_addr_t pa,
 	}
 	return 0;
 }
+
+int __weak boot_mode_is_closed(void)
+{
+	return 0;
+}

ricardosalveti/u-boot

commit 09243c3fac3d15942af134a48170dc3d7bab19f4

[FIO extras-squash] cmd: fiovb: fix using undeclared function boot_mode_is_closed()
Include a header file with the declaration of boot_mode_is_closed()
function, fixing a compiler warning [1].

[1]
cmd/fiovb.c: In function ‘do_fiovb_init’:
  CC      drivers/core/ofnode.o
cmd/fiovb.c:23:7: warning: implicit declaration of function ‘boot_mode_is_closed’ [-Wimplicit-function-declaration]
   23 |  if (!boot_mode_is_closed())
      |       ^~~~~~~~~~~~~~~~~~~

Fixes: 545d964536 ("[FIO extras] fiovb: return error on init if board not closed")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

cmd/fiovb.c
@@ -10,6 +10,7 @@
 #include <malloc.h>
 #include <mmc.h>
 #include <fiovb.h>
+#include <asm/arch/sys_proto.h>
 
 static struct fiovb_ops *fiovb_ops;
 