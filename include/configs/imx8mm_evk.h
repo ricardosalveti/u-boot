igoropaniuk/u-boot-1

[FIO internal] imx8mm_evk: fix build issues when SPL_DM=y
1. Force disable DM_I2C for the SPL build (workaround)
2. Disable usage of TCPC from the board file for the SPL build, as
from board/freescale/common/Makefile:

ifndef CONFIG_SPL_BUILD
obj-$(CONFIG_USB_TCPC) += tcpc.o
endif

Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

include/configs/imx8mm_evk.h
@@ -33,6 +33,7 @@
 #define CONFIG_POWER
 #define CONFIG_POWER_I2C
 #define CONFIG_POWER_BD71837
+#undef CONFIG_DM_I2C
 
 #define CONFIG_SYS_I2C
 

ricardosalveti/u-boot

1. Force disable SPL_DM_I2C for the SPL build (workaround)
Co-depeloped-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
(cherry picked from commit 0b43611a1faf41cf9b594e5b00dd14b1a6e56e3c)

include/configs/imx8mm_evk.h
@@ -28,6 +28,7 @@
 #define CFG_MALLOC_F_ADDR		0x930000
 /* For RAW image gives a error info not panic */
 
+#undef CONFIG_SPL_DM_I2C
 #endif
 
 #define PHY_ANEG_TIMEOUT 20000